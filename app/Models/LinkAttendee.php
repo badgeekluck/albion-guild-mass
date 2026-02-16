f2b3205e7b1f12b185692e777988480b6021ea72
Harun Baş <badgeekluck>
2026-02-16 12:46:32 +0300

    fix model

--- app/Models/LinkAttendee.php
+++ app/Models/LinkAttendee.php
@@ -1,10 +1,22 @@
 <?php
+
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class LinkAttendee extends Model {
+class LinkAttendee extends Model
+{
+    use HasFactory;
+
     protected $guarded = [];
 
+    public function link()
+    {
+        return $this->belongsTo(SharedLink::class, 'link_id');
+    }
+    // ----------------------------------
+    
     public function user()
     {
         return $this->belongsTo(User::class);
