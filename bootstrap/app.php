859951fa0e46eadf1c0d01194457abde3adcb787
Harun Baş <harun.1996.187@gmail.com>
2026-02-18 13:59:57 +0300

    fix bootstrap for reverb

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withBroadcasting(
         __DIR__.'/../routes/channels.php',
-        ['prefix' => 'api', 'middleware' => ['api', 'auth:sanctum']],
+        ['prefix' => 'api', 'middleware' => ['web', 'auth']],
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
